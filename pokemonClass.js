d3904eb5225e579b0d93aecea8028212ebd84fdb
EstebanMa12 <desteban.maya@udea.edu.co>
2023-08-06 14:28:32 -0500

    class pokemon added !

---
+++ pokemonClass.js
@@ -0,0 +1,11 @@
+class Pokemon{
+    constructor(name, type, health){
+        this.name=name,
+        this.type=type,
+        this.health=health
+    }
+}
+
+
+let Pikachu = new Pokemon("Pikachu","Electric",100)
+console.log(Pikachu);
