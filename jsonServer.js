39aeeda07a8e9bf68fc405b7984c708b77f8d756
EstebanMa12 <desteban.maya@udea.edu.co>
2023-08-08 16:13:30 -0500

    Descanso xD

--- jsonServer.js
+++ jsonServer.js
@@ -1,4 +1,4 @@
-export function enviarDatosPokemon(pokemonInfo,URL) {
+/* export function enviarDatosPokemon(pokemonInfo,URL) {
     axios.post(URL,pokemonInfo)
     .then(response =>{
         console.log('Datos enviados al servidor:', response.data);
@@ -6,4 +6,31 @@ export function enviarDatosPokemon(pokemonInfo,URL) {
     .catch(error =>{
         console.error("Error al enviar los datos al servidor", error);
     })
+} */
+
+export function enviarDatosPokemon(pokemonInfo, URL) {
+    axios.get(URL)
+        .then(response => {
+            const data = response.data;
+
+            if (Array.isArray(data.pokemons)) {
+                const updatedData = {
+                    ...data,
+                    pokemons: [...data.pokemons, pokemonInfo]
+                };
+
+                // Envía el objeto actualizado al servidor
+                return axios.put(URL, updatedData);
+            } else {
+                console.error('La propiedad "pokemons" no es una matriz en la respuesta JSON');
+            }
+        })
+        .then(response => {
+            if (response) {
+                console.log('Datos actualizados en el servidor:', response.data);
+            }
+        })
+        .catch(error => {
+            console.error('Error al actualizar datos en el servidor:', error);
+        });
 }
